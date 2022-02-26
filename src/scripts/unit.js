b48e860ff850d21d8ebcb57ef510c86ed0358df7
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-02-26 16:51:11 -0500

    Created Skeleton of Unit Class

--- src/scripts/unit.js
+++ src/scripts/unit.js
@@ -1,11 +1,9 @@
-import Grid from "./grid.js";
-
 class Unit{
   constructor(options){
     // Position is in [0,0] - 2d array format, is converted mathmatically
     // into the center of a square on the board.
     this.pos = options.pos;
-    this.grid = options.grid;
+    this.canvas = options.canvas;
     this.health = options.health;
     this.movementRange = options.movementRange;
     this.shootingRange = options.shootingRange;
@@ -13,12 +11,16 @@ class Unit{
   }
 
   draw(){
-    let canvas_array = document.getElementsByClassName("game-board");
-    let ctx = canvas.getContext("2d");
-    // Get the center of the square of the units position
+    let ctx = this.canvas.getContext("2d");
+    let color = yellow;
+    if (this.enemy){let color = green};
+    ctx.fillStyle = color;
+     // Get the center of the square of the units position
     let center_x = this.pos[0] * 80 + 40;
     let center_y = this.pos[1] * 80 + 40;
-    
+    ctx.beginPath();
+    ctx.arc(center_x, center_y, 20, 0, 2 * Math.PI, false);
+    ctx.fill();
   }
 };
 
