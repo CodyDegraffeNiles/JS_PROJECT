2c2fa42df7ffb3f6597d06c2cc00d377c4d1dd89
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-02-26 17:18:35 -0500

    Built basic draw function for units

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import Unit from "./scripts/unit.js";
 
 document.addEventListener("DOMContentLoaded", () =>{
   console.log("working :)")
+  window.Grid = Grid;
+  window.Unit = Unit;
 
   const grid = new Grid()
   grid.draw();
-  let canvas_array = document.getElementsByClassName("game-board");
-  let canvas = canvas_array[0];
-  let ctx = canvas.getContext("2d");
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = "red";
-  ctx.beginPath();
-  ctx.moveTo(200,200);
-  ctx.lineTo(240,240);
-  ctx.stroke();
+
+  /// Testing Code;
+  // let canvas_array = document.getElementsByClassName("game-board");
+  // let canvas = canvas_array[0];
+  // let ctx = canvas.getContext("2d");
+  // ctx.lineWidth = 2;
+  // ctx.strokeStyle = "red";
+  // ctx.beginPath();
+  // ctx.moveTo(200,200);
+  // ctx.lineTo(240,240);
+  // ctx.stroke();
 
 })
--- src/scripts/unit.js
+++ src/scripts/unit.js
@@ -12,8 +12,8 @@ class Unit{
 
   draw(){
     let ctx = this.canvas.getContext("2d");
-    let color = yellow;
-    if (this.enemy){let color = green};
+    let color = "yellow";
+    if (this.enemy){color = "green"};
     ctx.fillStyle = color;
      // Get the center of the square of the units position
     let center_x = this.pos[0] * 80 + 40;
@@ -22,6 +22,22 @@ class Unit{
     ctx.arc(center_x, center_y, 20, 0, 2 * Math.PI, false);
     ctx.fill();
   }
+
+  move([x,y]){
+    this.pos[0] = this.pos[0] + x;
+    this.pos[1] = this.pos[1] + y;
+  }
 };
 
 export default Unit;
+
+/// Example object for testing purposes
+
+new Unit({
+  pos: [4,4],
+  canvas: (document.getElementsByClassName("game-board"))[0],
+  health: 40,
+  movementRange: 2,
+  shootingRange: 2,
+  enemy: false
+})
