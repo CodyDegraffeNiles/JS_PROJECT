3834654699a2350e2eaa433abf8389cf82247824
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-02-27 23:12:49 -0500

    Created working canvas event listener

--- src/scripts/grid_view.js
+++ src/scripts/grid_view.js
@@ -4,6 +4,7 @@ import Unit from "./unit.js";
 class GridView {
   constructor(grid){
     this.grid = grid;
+    this.boundClick = this.handleClick.bind(this);
   };
 
   // build intital board
@@ -19,15 +20,33 @@ class GridView {
 
   bindClick(){
     let canvas = (document.getElementsByClassName("game-board")[0]);
-    canvas.addEventListener("click", this.handleClick);
+    canvas.addEventListener("click", this.boundClick);
   }
 
   handleClick(e){
     e.preventDefault();
     e.stopPropagation();
-    alert("Testing");
+    let canvas = (document.getElementsByClassName("game-board")[0]);
+    let cavansLeft = canvas.offsetLeft + canvas.clientLeft;
+    let canvasRight = canvas.offsetTop + canvas.clientTop;
+    let x = e.pageX - cavansLeft;
+    let y = e.pageY - canvasRight;
+    if(this.findUnit(x,y)){
+      console.log("boboLand");
+    }
+  };
+  // On click determines if a unit was clicked
+  findUnit(x,y){
+    let foundUnit = false;
+    this.grid.units.forEach(unit => {
+      if (x > unit.pos[0] * 80 && x < unit.pos[0] * 80 + 80 &&
+        y > unit.pos[1] * 80 && y < unit.pos[1] * 80 + 80) {
+        alert("clicked a unit!")
+        foundUnit = true;
+      }
+    })
+    return foundUnit;
   }
-
 }
 
 
