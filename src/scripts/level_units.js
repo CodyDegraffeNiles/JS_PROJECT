c2e7e14349f0134afcebf2f4801f6a19c7931d38
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-03-03 04:15:29 -0500

    Reworking seed data

--- src/scripts/level_units.js
+++ src/scripts/level_units.js
@@ -2,60 +2,71 @@ import Cover from "./cover.js";
 import Unit from "./unit.js";
 
 const levelUnits = [new Unit({
-  pos: [0,0],
+  pos: [2,6],
   health: 100,
-  movementRange: 2,
+  movementRange: 3,
   shootingRange: 4,
-  shootingPower: 50,
+  shootingPower: 75,
   enemy: false,
-  name: "Ajax"
+  name: "Tallarn 42nd" 
 }),
 
   new Unit({
-  pos: [7, 0],
+  pos: [5, 7],
   health: 100,
-  movementRange: 2,
+  movementRange: 3,
   shootingRange: 4,
-  shootingPower: 50,
+  shootingPower: 75,
   enemy: false,
-  name: "Renektus"
+  name: "Catachan 22nd"
 
 }),
 
   new Unit({
-    pos: [6, 0],
-    health: 50,
+    pos: [3, 3],
+    health: 75,
     movementRange: 2,
-    shootingRange: 4,
+    shootingRange: 7,
     shootingPower: 100,
     enemy: true,
-    name: "Tuska"
+    name: "Tzeentch's Scheme"
 }),
 
   new Unit({
-    pos: [0, 1],
-    health: 50,
+    pos: [2, 0],
+    health: 75,
     movementRange: 2,
-    shootingRange: 4,
+    shootingRange: 7,
     shootingPower: 100,
     enemy: true,
-    name: "DemonaKilla"
+    name: "Khorne's Glory"
   }),
 
   new Unit({
-    pos: [7, 7],
-    health: 50,
+    pos: [7, 4],
+    health: 75,
     movementRange: 2,
-    shootingRange: 4,
+    shootingRange: 7,
     shootingPower: 100,
     enemy: true,
-    name: "Testing"
+    name: "Nurgle's Love"
+  }),
+
+  new Unit({
+    pos: [6, 1],
+    health: 75,
+    movementRange: 2,
+    shootingRange: 7,
+    shootingPower: 100,
+    enemy: true,
+    name: "Slaanesh's Desire"
   }),
 
 new Cover([0,2]),
 new Cover([2,4]),
-new Cover([3,4]),
-new Cover([7,5])
+new Cover([7,5]),
+new Cover([4,6]),
+new Cover([6,2])
 ]
 
 export default levelUnits;
