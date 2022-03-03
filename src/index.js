cbf7a4cc6c7344c48ff64253b0b18500260a6721
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-03-03 15:37:19 -0500

    Minor styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Grid from "./scripts/grid.js";
-import Unit from "./scripts/unit.js";
 import GridView from "./scripts/grid_view.js"
-import Cover from "./scripts/cover.js";
 import AI from "./scripts/AI.js";
 
 document.addEventListener("DOMContentLoaded", () =>{
